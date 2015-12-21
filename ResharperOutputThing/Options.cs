385557f524f7573cf8cae0b97a894b3b26bdb8c7
Bert Vanpeteghem <vanpeteghem.bert@gmail.com>
2015-12-21 12:58:29 +0100

    initial commit simple r# output to text thing

---
+++ ResharperOutputThing/Options.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandLine;
+using CommandLine.Text;
+
+namespace ResharperOutputThing
+{
+    public class Options
+    {
+        [ParserState]
+        public IParserState LastParserState { get; set; }
+
+        [Option(Required=true)]
+        public FilterStatus FilterStatus { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            var help = new HelpText();
+
+            if (this.LastParserState.Errors.Any())
+            {
+                var errors = help.RenderParsingErrorsText(this, 2);
+
+                if (!string.IsNullOrEmpty(errors))
+                {
+                    help.AddPreOptionsLine(string.Concat(Environment.NewLine, "ERROR(S):"));
+                    help.AddPreOptionsLine(errors);
+                }
+            }
+
+            return help;
+        }
+    }
+
+    public enum FilterStatus
+    {
+        Unknown,
+        Success,
+        Failed
+    }
+}
---
+++ ResharperOutputThing/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace ResharperOutputThing
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            try
+            {
+                Options o = new Options();
+
+                if (!CommandLine.Parser.Default.ParseArguments(args, o))
+                {
+                    EndCommand();
+                    return;
+                }
+
+                var clipboardText = Clipboard.GetText();
+
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(clipboardText);
+
+                var xpathQuery = string.Format("//node[@kind='MSTest Test' and @status='{0}']", o.FilterStatus);
+
+                var nodes = doc.SelectNodes(xpathQuery);
+
+                var filename = string.Format("c:\\temp\\testoutput-{0}.txt", DateTime.Now.ToString("yyyyMMdd-HHmmss"));
+
+                File.AppendAllText(
+                        filename,
+                        Environment.NewLine + "Printing tests with Status: " + o.FilterStatus);
+
+                foreach (XmlNode node in nodes)
+                {
+                    var line = node.Attributes["name"].Value;
+                    
+                    Console.WriteLine(line);
+                    
+                    File.AppendAllText(
+                        filename, 
+                        Environment.NewLine + line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            EndCommand();
+        }
+
+        private static void EndCommand()
+        {
+            Console.WriteLine(Environment.NewLine + "Ready. Press enter.");
+            Console.ReadLine();
+        }
+    }
+}
